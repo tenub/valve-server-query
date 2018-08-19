1a6731bba17fdcd8d31e2fa30baf088f9e608e8a
Joseph Chrzan <jchrzan1@gmail.com>
2018-08-19 00:32:20 -0400

    add other query case skeletons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// @flow
-
 const dns = require('dns');
 const { createSocket } = require('dgram');
 const EventEmitter = require('events');
@@ -23,6 +21,14 @@ class RequestPacket {
     switch (type) {
       case 'info':
         return this.info();
+      case 'player':
+        return this.player();
+      case 'rules':
+        return this.rules();
+      case 'challenge':
+        return this.getchallenge();
+      case 'ping':
+        return this.ping();
     }
   }
 
@@ -33,6 +39,44 @@ class RequestPacket {
     index = packet.writeUInt8(0x54, index);
     index += packet.write('Source Engine Query', index);
     index = packet.writeUInt8(0x00, index);
+
+    return packet;
+  }
+
+  player(challenge = -1) {
+    const packet = Buffer.alloc(9);
+
+    let index = packet.writeInt32LE(-1, 0);
+    index = packet.writeUInt8(0x55, index);
+    index = packet.writeInt32LE(challenge, index);
+
+    return packet;
+  }
+
+  rules(challenge = -1) {
+    const packet = Buffer.alloc(9);
+
+    let index = packet.writeInt32LE(-1, 0);
+    index = packet.writeUInt8(0x56, index);
+    index = packet.writeInt32LE(challenge, index);
+
+    return packet;
+  }
+
+  getchallenge() {
+    const packet = Buffer.alloc(5);
+
+    let index = this.packet.writeInt32LE(-1, 0);
+    index = packet.writeUInt8(0x57, index);
+
+    return packet;
+  }
+
+  ping() {
+    const packet = Buffer.alloc(5);
+
+    let index = packet.writeInt32LE(-1, 0);
+    index = packet.writeUInt8(0x69, index);
 
     return packet;
   }
@@ -102,7 +146,7 @@ class ServerQuery extends EventEmitter {
     this.handleSocketError = this.handleSocketError.bind(this);
     this.handleSocketMessage = this.handleSocketMessage.bind(this);
     this.sendRequests = this.sendRequests.bind(this);
-    this.sendInfoPacket = this.sendInfoPacket.bind(this);
+    this.requestInfo = this.requestInfo.bind(this);
     this.sendPacket = this.sendPacket.bind(this);
 
     // save starting timestamp just before opening a socket connection
@@ -151,11 +195,213 @@ class ServerQuery extends EventEmitter {
     await this.resolveConnections();
 
     // send info query for each connection
-    await Promise.all(this.connections.map(this.sendInfoPacket));
+    await Promise.all(this.connections.map(this.requestInfo));
   }
 
   handleSocketError(err) {
     this.emit('error', err);
+  }
+
+  parseInfo(packet) {
+    const protocol = packet.readInt(1);
+    const name = packet.readString();
+    const map = packet.readString();
+    const folder = packet.readString();
+    const game = packet.readString();
+    const id = packet.readInt(2);
+    const players = packet.readInt(1);
+    const maxplayers = packet.readInt(1);
+    const bots = packet.readInt(1);
+    const type = packet.readChar();
+    const environment = packet.readChar();
+    const visibility = packet.readInt(1);
+    const vac = packet.readInt(1);
+
+    let data = {
+      protocol,
+      name,
+      map,
+      folder,
+      game,
+      id,
+      players,
+      maxplayers,
+      bots,
+      type,
+      environment,
+      visibility,
+      vac
+    };
+
+    // The Ship
+    if (id === 2400) {
+      const mode = packet.readInt(1);
+      const witnesses = packet.readInt(1);
+      const duration = packet.readInt(1);
+
+      data.mode = mode;
+      data.witnesses = witnesses;
+      data.duration = duration;
+    }
+
+    const version = packet.readString();
+    data.version = version;
+
+    const flag = packet.readInt(1);
+
+    if (flag & 0x80) {
+      const port = packet.readInt(2);
+      data.port = port;
+    }
+
+    if (flag & 0x10) {
+      const steamid = packet.readInt(8).toString();
+      data.steamid = steamid;
+    }
+
+    if (flag & 0x40) {
+      const spectator = {
+        port: packet.readInt(2),
+        name: packet.readString()
+      };
+
+      data.spectator = spectator;
+    }
+
+    if (flag & 0x20) {
+      const keywords = packet.readString();
+      data.keywords = keywords;
+    }
+
+    if (flag & 0x01) {
+      const gameid = packet.readInt(8).toString();
+      data.gameid = gameid;
+    }
+
+    return data;
+  }
+
+  parseInfoObsolete(packet) {
+    const address = packet.readString();
+    const name = packet.readString();
+    const map = packet.readString();
+    const folder = packet.readString();
+    const game = packet.readString();
+    const players = packet.readInt(1);
+    const maxplayers = packet.readInt(1);
+    const protocol = packet.readInt(1);
+    const type = packet.readChar();
+    const environment = packet.readChar();
+    const visibility = packet.readInt(1);
+    const mod = packet.readInt(1);
+
+    const data = {
+      address,
+      name,
+      map,
+      folder,
+      game,
+      players,
+      maxplayers,
+      protocol,
+      type,
+      environment,
+      visibility,
+      mod
+    };
+
+    if (data.mod === 1) {
+      const mod = {
+        link: packet.readString(),
+        downloadlink: packet.readString()
+      };
+
+      packet.readInt(1); // null byte
+
+      mod.version = packet.readInt(4);
+      mod.size = packet.readInt(4);
+      mod.type = packet.readInt(1);
+      mod.dll = packet.readInt(1);
+
+      data.mod = mod;
+    }
+
+    const vac = packet.readInt(1);
+    const bots = packet.readInt(1);
+
+    data.vac = vac;
+    data.bots = bots;
+
+    return data;
+  }
+
+  parsePlayers(packet, flag = false) {
+    const players = [];
+
+    let numplayers = this.readInt(1);
+
+    if (flag) {
+      while (numplayers > 0 && this.index < this.data.length) {
+        players.push({
+          index: this.readInt(1),
+          name: this.readString(),
+          score: this.readInt(4),
+          duration: this.readFloat(),
+          deaths: this.readInt(4),
+          money: this.readInt(4)
+        });
+
+        numplayers -= 1;
+      }
+    } else {
+      while (numplayers > 0 && this.index < this.data.length) {
+        players.push({
+          index: this.readInt(1),
+          name: this.readString(),
+          score: this.readInt(4),
+          duration: this.readFloat()
+        });
+
+        numplayers -= 1;
+      }
+    }
+
+    const data = { players };
+
+    return data;
+  }
+
+  parseRules(packet) {
+    const rules = [];
+
+    let numrules = packet.readInt(2);
+
+    while (numrules > 0 && packet.index < packet.data.length) {
+      const name = packet.readString();
+      const value = packet.readString();
+
+      rules.push({ name, value });
+
+      numrules -= 1;
+    }
+
+    const data = { rules };
+
+    return data;
+  }
+
+  parseChallenge(packet) {
+    const challenge = packet.readInt(4);
+    const data = { challenge };
+
+    return data;
+  }
+
+  parsePing(packet) {
+    const ping = packet.readString();
+    const data = { ping };
+
+    return data;
   }
 
   handleSocketMessage(msg, { address, port }) {
@@ -176,153 +422,65 @@ class ServerQuery extends EventEmitter {
     const packet = new ResponsePacket(msg);
 
     // read response type
-    const headerType = packet.readInt(4);
+    const headerFormat = packet.readInt(4);
 
-    if (headerType === -1) { // simple response format
-      const headerInfo = packet.readInt(1);
+    switch (headerFormat) {
+      case -1: // simple response format
+        break;
+      case -2: // multi-packet response format
+        break;
+      default:
+        this.emit('error', new Error(`Expected a valid response header but got "${headerFormat}.`));
+        return false;
+    }
 
-      if (headerInfo === 0x49) { // standard info response
-        const protocol = packet.readInt(1);
-        const name = packet.readString();
-        const map = packet.readString();
-        const folder = packet.readString();
-        const game = packet.readString();
-        const id = packet.readInt(2);
-        const players = packet.readInt(1);
-        const maxplayers = packet.readInt(1);
-        const bots = packet.readInt(1);
-        const type = packet.readChar();
-        const environment = packet.readChar();
-        const visibility = packet.readInt(1);
-        const vac = packet.readInt(1);
+    const headerType = packet.readInt(1);
 
-        let data = {
-          protocol,
-          name,
-          map,
-          folder,
-          game,
-          id,
-          players,
-          maxplayers,
-          bots,
-          type,
-          environment,
-          visibility,
-          vac
-        };
-
-        // The Ship
-        if (id === 2400) {
-          const mode = packet.readInt(1);
-          const witnesses = packet.readInt(1);
-          const duration = packet.readInt(1);
-
-          data.mode = mode;
-          data.witnesses = witnesses;
-          data.duration = duration;
-        }
-
-        const version = packet.readString();
-        data.version = version;
-
-        const flag = packet.readInt(1);
-
-        if (flag & 0x80) {
-          const port = packet.readInt(2);
-          data.port = port;
-        }
-
-        if (flag & 0x10) {
-          const steamid = packet.readInt(8).toString();
-          data.steamid = steamid;
-        }
-
-        if (flag & 0x40) {
-          const spectator = {
-            port: packet.readInt(2),
-            name: packet.readString()
-          };
-
-          data.spectator = spectator;
-        }
-
-        if (flag & 0x20) {
-          const keywords = packet.readString();
-          data.keywords = keywords;
-        }
-
-        if (flag & 0x01) {
-          const gameid = packet.readInt(8).toString();
-          data.gameid = gameid;
-        }
-
-        this.emit('info', data);
-
-        this.connections[connectionIndex].info = data;
-      } else if (headerInfo === 0x6D) { // obsolete GoldSource response
-        const address = packet.readString();
-        const name = packet.readString();
-        const map = packet.readString();
-        const folder = packet.readString();
-        const game = packet.readString();
-        const players = packet.readInt(1);
-        const maxplayers = packet.readInt(1);
-        const protocol = packet.readInt(1);
-        const type = packet.readChar();
-        const environment = packet.readChar();
-        const visibility = packet.readInt(1);
-        const mod = packet.readInt(1);
-
-        const data = {
-          address,
-          name,
-          map,
-          folder,
-          game,
-          players,
-          maxplayers,
-          protocol,
-          type,
-          environment,
-          visibility,
-          mod
-        };
-
-        if (data.mod === 1) {
-          const mod = {
-            link: packet.readString(),
-            downloadlink: packet.readString()
-          };
-
-          packet.readInt(1); // null byte
-
-          mod.version = packet.readInt(4);
-          mod.size = packet.readInt(4);
-          mod.type = packet.readInt(1);
-          mod.dll = packet.readInt(1);
-
-          data.mod = mod;
-        }
-
-        const vac = packet.readInt(1);
-        const bots = packet.readInt(1);
-
-        data.vac = vac;
-        data.bots = bots;
-
-        this.emit('info', data);
-
-        this.connections[connectionIndex].info = data;
-      } else {
-        this.emit('error', new Error(`Expected a valid info header value of "0x49" or "0x6D" but got "${headerInfo}.`));
-      return false;
+    switch (headerType) {
+      // challenge response
+      case 0x41: {
+        const data = this.parseChallenge(packet);
+        this.emit('challenge', data);
+        this.connections[connectionIndex].challenge = data;
+        break;
       }
-    } else if (headerType === -2) { // multi-packet response format
 
-    } else {
-      this.emit('error', new Error(`Expected a valid response header value of "-1" or "-2" but got "${headerType}.`));
-      return false;
+      // standard info response
+      case 0x49: {
+        const data = this.parseInfo(packet);
+        this.emit('info', data);
+        this.connections[connectionIndex].info = data;
+        break;
+      }
+
+      // obsolete GoldSource info response
+      case 0x6D: {
+        const data = this.parseInfoObsolete(packet);
+        this.emit('info', data);
+        this.connections[connectionIndex].info = data;
+        break;
+      }
+
+      // player response
+      case 0x44: {
+        const data = this.parsePlayer(packet);
+        this.emit('player', data);
+        this.connections[connectionIndex].players = data;
+        break;
+      }
+
+      // rules response
+      case 0x45: {
+        const data = this.parseRules(packet);
+        this.emit('rules', data);
+        this.connections[connectionIndex].rules = data;
+        break;
+      }
+
+      // unknown response
+      default:
+        this.emit('error', new Error(`Expected a valid type header but got "${headerType}".`));
+        return false;
     }
 
     if (this.connections.every(({ info }) => info)) {
@@ -350,10 +508,24 @@ class ServerQuery extends EventEmitter {
     });
   }
 
-  async sendInfoPacket({ host, port }) {
+  async requestInfo({ host, port }, i) {
     const packet = new RequestPacket('info');
 
     try {
+      this.connections[i]._type = 'info';
+      await this.sendPacket(packet, port, host);
+      return true;
+    } catch (err) {
+      this.emit('error', err);
+      return false;
+    }
+  }
+
+  async requestPlayer({ host, port }, i) {
+    const packet = new RequestPacket('player');
+
+    try {
+      this.connections[i]._type = 'player';
       await this.sendPacket(packet, port, host);
       return true;
     } catch (err) {
